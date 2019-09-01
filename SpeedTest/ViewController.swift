8df24b76ecd06343717c493ad1fb211a26adc33c
halsey <wsh940808@163.com>
2019-09-01 17:49:40 +0800

    创建了项目

---
+++ SpeedTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedTest
+//
+//  Created by Wushhhhhh on 2019/9/1.
+//  Copyright © 2019 wushhhhhh. All rights reserved.
+//ı
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
